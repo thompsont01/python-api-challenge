c654159816999b366a38aa29de2411620949bbfa
Thompson Tang <thompsontang@thompsons-mbp.attlocal.net>
2020-07-13 19:11:58 -0700

    Added final touches

--- Challenges/VacationPy/api_keys.py
+++ Challenges/VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cbebf54a5406795ca45ec3ae165e7b1d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBgGgVu-cXpF75ulvuPzzUfOnB06VdOLRE"
